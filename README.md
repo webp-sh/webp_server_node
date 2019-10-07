37997576866cdeea646564b218a974aa0bab2f86
n0vad3v <n0vad3v@riseup.net>
2019-10-07 14:19:03 +0800

    Fix cache bug.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # WebP Server
 
+**THIS PROJECT IS UNDER EARLY DEVELOPMENT, DON'T USE IT ON PRODUCTION ENVIORMENT.**
+
 This is a NodeJS Server based on Express and cwebp, which allows you to serve WebP images on the fly.
 
 > e.g When you visit `https://a.com/1.jpg`，it will serve as `image/webp` without changing the URL.
 
+## Usage
+
+Define your pics folder on `index.js` (for instance there is a `1.jpg` in the related pic folder):
+
+```
+const IMG_PATH = "/PATH/TO/pics"
+```
+
+run the APP and let Nginx to `proxy_pass http://localhost:3000/;`
--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const IMG_PATH = "/PATH/TO/pics"
 
 // Define the catch-all interface
 app.get('/*',function(req,res){
+		// Get requested URL and concatenate absolute path and URL
         var requested_img_path = path.join(IMG_PATH, req.originalUrl);
-        var store_path = path.parse(requested_img_path)
 
-        var cache_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0]);
-        console.log("cache "+cache_path);
+        var cache_path = path.join(__dirname,"webp/" + path.dirname(req.originalUrl));
+		console.log("CACHE PATH is " + cache_path)
         var cache_image_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0] + ".webp");
 
 		// Make Cache path for the processed images
@@ -33,6 +33,5 @@ app.get('/*',function(req,res){
                 console.log("Convert" + requested_img_path);
                 console.log(status,error);
         })
-
 })
 app.listen(port, () => console.log(`WebP Server is listening on port ${port}!`))
