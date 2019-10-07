5aa7e5e9e49e96eea7c3492a705afb196498776d
n0vad3v <n0vad3v@riseup.net>
2019-10-07 12:30:39 +0800

    Added README

---
+++ README.md
@@ -0,0 +1,6 @@
+# WebP Server
+
+This is a NodeJS Server based on Express and cwebp, which allows you to serve WebP images on the fly.
+
+> e.g When you visit `https://a.com/1.jpg`，it will serve as `image/webp` without changing the URL.
+
--- index.js
+++ index.js
@@ -3,25 +3,36 @@ var webp = require('webp-converter')
 const path = require('path')
 const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 3333
 
+const IMG_PATH = "/PATH/TO/pics"
 
+// Define the catch-all interface
 app.get('/*',function(req,res){
-	var requested_img_path = path.join(__dirname, req.originalUrl);
-	var store_path = path.parse(requested_img_path)
+        var requested_img_path = path.join(IMG_PATH, req.originalUrl);
+        var store_path = path.parse(requested_img_path)
 
-	var cache_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0] + ".webp");
-	if(fs.existsSync(cache_path)){
-		res.sendFile(cache_path)
-		return;
-	}
-	console.log(cache_path);
-	
-	var webp_image = webp.cwebp(requested_img_path,cache_path,"-q 80",function(status,error){
-		res.sendFile(cache_path);
-		console.log("Convert" + requested_img_path);
-		console.log(status,error);
-	})
+        var cache_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0]);
+        console.log("cache "+cache_path);
+        var cache_image_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0] + ".webp");
+
+		// Make Cache path for the processed images
+        if(!fs.existsSync(cache_path)){
+                fs.mkdirSync(cache_path, { recursive: true });
+        }
+
+		// Hit Cache, then just send the processed images
+        if(fs.existsSync(cache_image_path)){
+                console.log("Sending")
+                res.sendFile(cache_image_path)
+                return;
+        }
+
+        var webp_image = webp.cwebp(requested_img_path,cache_image_path,"-q 80",function(status,error){
+                res.sendFile(cache_image_path);
+                console.log("Convert" + requested_img_path);
+                console.log(status,error);
+        })
 
 })
-app.listen(port, () => console.log(`WebP Server listening on port ${port}!`))
+app.listen(port, () => console.log(`WebP Server is listening on port ${port}!`))
