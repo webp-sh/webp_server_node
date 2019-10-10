462d27ad93762ac028422ed04d2bae0793a56c4e
n0vad3v <n0vad3v@riseup.net>
2019-10-10 10:39:36 +0800

    Fix weird indent problem.

--- index.js
+++ index.js
@@ -11,26 +11,26 @@ const IMG_PATH = "/PATH/TO/pics"
 app.get('/*',function(req,res){
 		// Get requested URL and concatenate absolute path and URL
         var requested_img_path = path.join(IMG_PATH, req.originalUrl);
-		var ua = req.headers['user-agent']
+	var ua = req.headers['user-agent']
 
-		// Check if its Safari, and send the original image
-		console.log("UA is "+ ua);
-		if(ua.includes('Safari') && !ua.includes('Chrome') && !ua.includes('Firefox')){
-			console.log("Gotcha Safari!");
-			res.sendFile(requested_img_path);
-			return;
-		}
+	// Check if its Safari, and send the original image
+	console.log("UA is "+ ua);
+	if(ua.includes('Safari') && !ua.includes('Chrome') && !ua.includes('Firefox')){
+		console.log("Gotcha Safari!");
+		res.sendFile(requested_img_path);
+		return;
+	}
 
         var cache_path = path.join(__dirname,"webp/" + path.dirname(req.originalUrl));
 		console.log("CACHE PATH is " + cache_path)
         var cache_image_path = path.join(__dirname,"webp/" + req.originalUrl.split(".")[0] + ".webp");
 
-		// Make Cache path for the processed images
+	// Make Cache path for the processed images
         if(!fs.existsSync(cache_path)){
                 fs.mkdirSync(cache_path, { recursive: true });
         }
 
-		// Hit Cache, then just send the processed images
+	// Hit Cache, then just send the processed images
         if(fs.existsSync(cache_image_path)){
                 console.log("Sending")
                 res.sendFile(cache_image_path)
