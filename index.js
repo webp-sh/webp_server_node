3427bb09b9d0e61c144671cba9c431b978eba1e9
n0vad3v <n0vad3v@riseup.net>
2019-10-12 20:07:25 +0800

    Add: Check the original file's existence. Move to LF as EOL.

--- index.js
+++ index.js
@@ -15,20 +15,27 @@ const app = express();
 // Define the catch-all interface
 app.get('/*', (req, res) => {
     // Get requested URL and concatenate absolute path and URL
-    let requested_img_path = path.join(IMG_PATH, req.originalUrl);
-    let ua = req.headers['user-agent'];
+    let requested_img_path = path.join(IMG_PATH, req.originalUrl); // Will get /PATH/TO/pics/images/tsuki.jpg
+    let cache_path = path.join(__dirname, "webp/" + path.dirname(req.originalUrl)); // Will get /PATH/TO/webp_server/webp/images
+    let cache_img_path = path.join(__dirname, "webp/" + req.originalUrl.split(".")[0] + ".webp"); // Will get /PATH/TO/webp_server/webp/images/tsuki.webp
+
+    // Check the original image for existence
+    if(!fs.existsSync(requested_img_path)){
+        // The original image doesn't exist, check the webp image, delete if processed.
+        if(fs.existsSync(cache_img_path)){
+            fs.unlink(cache_img_path);
+        }
+        res.status(404).send("Not Found.");
+    }
 
     // Check if its Safari, and send the original image
+    let ua = req.headers['user-agent'];
     console.log("UA is " + ua);
     if (ua.includes('Safari') && !ua.includes('Chrome') && !ua.includes('Firefox')) {
         console.log("Gotcha Safari!");
         res.sendFile(requested_img_path);
         return;
     }
-
-    let cache_path = path.join(__dirname, "webp/" + path.dirname(req.originalUrl));
-    console.log("CACHE PATH is " + cache_path);
-    let cache_image_path = path.join(__dirname, "webp/" + req.originalUrl.split(".")[0] + ".webp");
 
     // Make Cache path for the processed images
     if (!fs.existsSync(cache_path)) {
@@ -38,14 +45,14 @@ app.get('/*', (req, res) => {
     }
 
     // Hit Cache, then just send the processed images
-    if (fs.existsSync(cache_image_path)) {
+    if (fs.existsSync(cache_img_path)) {
         console.log("Sending");
-        res.sendFile(cache_image_path);
+        res.sendFile(cache_img_path);
         return;
     }
 
-    let webp_image = webp.cwebp(requested_img_path, cache_image_path, "-q 80", function (status, error) {
-        res.sendFile(cache_image_path);
+    let webp_image = webp.cwebp(requested_img_path, cache_img_path, "-q 80", function (status, error) {
+        res.sendFile(cache_img_path);
         console.log("Convert" + requested_img_path);
         console.log(status, error);
     })
