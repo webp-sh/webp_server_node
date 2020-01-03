3aa297d9cea6c3020d0adcdf88c4e38db33a3f62
n0vad3v <n0vad3v@riseup.net>
2020-01-03 16:14:35 +0800

    We should allow original webp

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     HOST: '127.0.0.1',
     PORT: 3333,
     IMG_PATH: "pics",
-    ALLOWED_TYPES: ['jpg','png','jpeg']
+    ALLOWED_TYPES: ['jpg','png','jpeg','webp']
 };
 
 module.exports = config;
